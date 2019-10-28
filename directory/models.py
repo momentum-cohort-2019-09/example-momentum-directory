3389fc1a9b89ce3738fce4a3c80d0ab6cd98ef95
Clinton Dreisbach <clinton@dreisbach.us>
2019-10-28 11:15:45 -0400

    Create initial models for directory

--- directory/models.py
+++ directory/models.py
@@ -1,6 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
-    pass
+    is_momentum_staff = models.BooleanField(default=False)
+    cohort = models.ForeignKey(to='Cohort',
+                               on_delete=models.SET_NULL,
+                               blank=True,
+                               null=True,
+                               related_name='members')
+    specialities = models.ManyToManyField(to='Technology',
+                                          blank=True,
+                                          related_name='specialists')
+    interests = models.ManyToManyField(to='Technology',
+                                       blank=True,
+                                       related_name='enthusiasts')
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Cohort(models.Model):
+    name = models.CharField(max_length=255)
+    start_date = models.DateField()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    members = models.ManyToManyField(to=User,
+                                     related_name='projects',
+                                     blank=True)
+    technologies = models.ManyToManyField(to=Technology,
+                                          related_name='projects',
+                                          blank=True)
+    video_url = models.URLField(blank=True, null=True)
+    repo_url = models.URLField(blank=True, null=True)
+    demo_url = models.URLField(blank=True, null=True)
+    final_project_for_cohort = models.ForeignKey(to=Cohort,
+                                                 on_delete=models.SET_NULL,
+                                                 related_name='final_projects',
+                                                 null=True,
+                                                 blank=True)
