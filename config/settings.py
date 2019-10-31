4fc8e8df08c52da4cd5ebc39a34bad94687b6735
Clinton Dreisbach <clinton@dreisbach.us>
2019-10-31 11:08:37 -0400

    Use PostgreSQL in development

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'directory',
+        'USER': 'directory',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
